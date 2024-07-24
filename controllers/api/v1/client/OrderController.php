2dc74173c6a70c300342c9d2eb1a7226c628b527
Andrew <code70@inbox.ru>
2024-07-24 21:45:30 +0700

    comment twilio temp

--- controllers/api/v1/client/OrderController.php
+++ controllers/api/v1/client/OrderController.php
@@ -269,27 +269,27 @@ class OrderController extends ClientController
                 $order->linkAll('attachments', $attachmentsToLink);
             }
 
-            $conversationManager = ChatConstructorService::createChatOrder(
-                Chat::GROUP_CLIENT_MANAGER,
-                [$user->id, $randomManager->id],
-                $order->id,
-            );
+            // $conversationManager = ChatConstructorService::createChatOrder(
+            //     Chat::GROUP_CLIENT_MANAGER,
+            //     [$user->id, $randomManager->id],
+            //     $order->id,
+            // );
 
-            NotificationConstructor::orderOrderCreated(
-                $order->manager_id,
-                $order->id,
-            );
-            // var_dump($conversationManager);
-            // code block where return error
-            if (!$conversationManager->success) {
-                $transaction?->rollBack();
-                return ApiResponse::codeErrors(
-                    $apiCodes->ERROR_SAVE,
-                    $conversationManager->reason,
-                );
-            }
+            // NotificationConstructor::orderOrderCreated(
+            //     $order->manager_id,
+            //     $order->id,
+            // );
 
-            $transaction?->commit();
+            // // code block where return error
+            // if (!$conversationManager->success) {
+            //     $transaction?->rollBack();
+            //     return ApiResponse::codeErrors(
+            //         $apiCodes->ERROR_SAVE,
+            //         $conversationManager->reason,
+            //     );
+            // }
+
+            // $transaction?->commit();
 
             return ApiResponse::byResponseCode(null, [
                 'info' => OrderOutputService::getEntity($order->id),
