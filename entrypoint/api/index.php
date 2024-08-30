0c4e0f14d54e711927acd245f228b8b079e0e823
Andrew <code70@inbox.ru>
2024-08-30 22:43:15 +0700

    enable debugger for all range of ips

--- entrypoint/api/index.php
+++ entrypoint/api/index.php
@@ -9,5 +9,6 @@ require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
 
-$config = require(__DIR__ . '/../../config/api.php');
+$config = require __DIR__ . '/../../config/api.php';
+
 (new yii\web\Application($config))->run();
