47ce60b318ba7598ece23338c2c31a8adec801d6
Andrew <code70@inbox.ru>
2024-09-09 23:02:17 +0700

    endpoint to reset app

--- controllers/RawController.php
+++ controllers/RawController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\components\ApiResponse;
 use app\models\Chat;
 use Yii;
 use yii\web\Controller;
@@ -15,6 +16,7 @@ use app\services\chat\ChatConstructorService;
 use app\services\twilio\TwilioService;
 use app\services\UserActionLogService as LogService;
 use Twilio\Rest\Client;
+
 
 class RawController extends Controller
 {
@@ -159,17 +161,80 @@ class RawController extends Controller
         $participiants = $client->conversations->v1->conversations($chatSid)->participants->read();
         return $participiants;
     }
-    public function actionTest()
+    public function actionResetApp()
     {
-        var_dump(
-            [
-                'asd' => 'asd',
-                'asd2' => 'asd2',
-                'asd3' => (object)['asd' => 'asd', 'asd2' => 'asd2'],
-                'asd4' => (object)['asd4' => 'asd4', 'asd5' => 'asd5'],
-                'asd6' => (object)['asd6' => 'asd6', 'asd7' => 'asd7'],
-            ]
-        );
-        die();
+        //define vars 
+        $db = Yii::$app->db;
+        $apiCodes = Order::apiCodes();
+        $attachmentsRoot = Yii::getAlias('@webroot/attachments');
+        $files = scandir($attachmentsRoot);
+
+        $tables = [
+            // 'app_option',
+            'attachment',
+            'buyer_delivery_offer',
+            'buyer_offer',
+            // 'category',
+            'chat',
+            'chat_translate',
+            'chat_user',
+            // 'delivery_point_address',
+            'feedback_buyer',
+            'feedback_buyer_link_attachment',
+            'feedback_product',
+            'feedback_product_link_attachment',
+            'feedback_user',
+            'feedback_user_link_attachment',
+            'fulfillment_inspection_report',
+            'fulfillment_marketplace_transaction',
+            'fulfillment_offer',
+            'fulfillment_packaging_labeling',
+            'fulfillment_stock_report',
+            'fulfillment_stock_report_link_attachment',
+            // 'migration',
+            'notification',
+            'order',
+            'order_distribution',
+            'order_link_attachment',
+            'order_rate',
+            'order_tracking',
+            'packaging_report_link_attachment',
+            'privacy_policy',
+            'product',
+            'product_inspection_report',
+            'product_link_attachment',
+            'product_stock_report',
+            'product_stock_report_link_attachment',
+            // 'rate',
+            // 'subcategory',
+            // 'type_delivery',
+            // 'type_delivery_link_category',
+            // 'type_delivery_link_subcategory',
+            // 'type_delivery_point',
+            // 'type_delivery_price',
+            // 'type_packaging',
+            // 'user',
+            'user_link_category',
+            'user_link_type_delivery',
+            'user_link_type_packaging',
+            // 'user_settings',
+            'user_verification_request',
+        ];
+        try {
+            $db->createCommand('SET FOREIGN_KEY_CHECKS = 0;')->execute();
+            foreach ($files as $file) {
+                if ($file !== '.' && $file !== '..') {
+                    unlink($attachmentsRoot . '/' . $file);
+                }
+            }
+
+            foreach ($tables as $table) {
+                $db->createCommand()->truncateTable($table)->execute();
+            }
+            $db->createCommand('SET FOREIGN_KEY_CHECKS = 1;')->execute();
+            return ApiResponse::byResponseCode($apiCodes->SUCCESS, ['message' => 'App reset successfully']);
+        } catch (\yii\db\Exception $e) {
+            return ApiResponse::byResponseCode($apiCodes->INTERNAL_ERROR, ['message' => $e->getMessage()]);
+        }
     }
 }
