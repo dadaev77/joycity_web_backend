31b4d13ff9c8f085a863f034c52a40d9bb78b931
Andrew <code70@inbox.ru>
2024-10-17 23:49:02 +0700

    fix delivery calculation

--- services/price/OrderDeliveryPriceService.php
+++ services/price/OrderDeliveryPriceService.php
@@ -11,6 +11,294 @@ use Yii;
 
 class OrderDeliveryPriceService extends PriceOutputService
 {
+
+    private static function typeDeliveryPriceConfig(int $typeDeliveryId): array
+    {
+        return [
+            1 => [],
+            2 => ['price' => 0],
+            3 => ['price' => 0],
+            4 => ['price' => 0],
+            5 => ['price' => 0],
+            6 => ['price' => 0],
+            7 => ['price' => 0],
+            8 => [ //slow car
+                [
+                    'id' => 1,
+                    'density_from' => 1000,
+                    'density_to' => 1000,
+                    'price' => 2.00,
+                ],
+                [
+                    'id' => 2,
+                    'density_from' => 900,
+                    'density_to' => 1000,
+                    'price' => 2.00,
+                ],
+                [
+                    'id' => 3,
+                    'density_from' => 800,
+                    'density_to' => 900,
+                    'price' => 2.00,
+                ],
+                [
+                    'id' => 4,
+                    'density_from' => 700,
+                    'density_to' => 800,
+                    'price' => 2.00,
+                ],
+                [
+                    'id' => 5,
+                    'density_from' => 600,
+                    'density_to' => 700,
+                    'price' => 2.10,
+                ],
+                [
+                    'id' => 6,
+                    'density_from' => 500,
+                    'density_to' => 600,
+                    'price' => 2.15,
+                ],
+                [
+                    'id' => 7,
+                    'density_from' => 400,
+                    'density_to' => 500,
+                    'price' => 2.20,
+                ],
+                [
+                    'id' => 8,
+                    'density_from' => 350,
+                    'density_to' => 400,
+                    'price' => 2.25,
+                ],
+                [
+                    'id' => 9,
+                    'density_from' => 300,
+                    'density_to' => 350,
+                    'price' => 2.30,
+                ],
+                [
+                    'id' => 10,
+                    'density_from' => 250,
+                    'density_to' => 300,
+                    'price' => 2.40,
+                ],
+                [
+                    'id' => 11,
+                    'density_from' => 200,
+                    'density_to' => 250,
+                    'price' => 2.45,
+                ],
+                [
+                    'id' => 12,
+                    'density_from' => 190,
+                    'density_to' => 200,
+                    'price' => 2.60,
+                ],
+                [
+                    'id' => 13,
+                    'density_from' => 180,
+                    'density_to' => 190,
+                    'price' => 2.70,
+                ],
+                [
+                    'id' => 14,
+                    'density_from' => 170,
+                    'density_to' => 180,
+                    'price' => 2.80,
+                ],
+                [
+                    'id' => 15,
+                    'density_from' => 160,
+                    'density_to' => 170,
+                    'price' => 2.90,
+                ],
+                [
+                    'id' => 16,
+                    'density_from' => 150,
+                    'density_to' => 160,
+                    'price' => 3.00,
+                ],
+                [
+                    'id' => 17,
+                    'density_from' => 140,
+                    'density_to' => 150,
+                    'price' => 3.10,
+                ],
+                [
+                    'id' => 18,
+                    'density_from' => 130,
+                    'density_to' => 140,
+                    'price' => 3.20,
+                ],
+                [
+                    'id' => 19,
+                    'density_from' => 120,
+                    'density_to' => 130,
+                    'price' => 3.30,
+                ],
+                [
+                    'id' => 20,
+                    'density_from' => 110,
+                    'density_to' => 120,
+                    'price' => 3.40,
+                ],
+                [
+                    'id' => 21,
+                    'density_from' => 100,
+                    'density_to' => 110,
+                    'price' => 3.50,
+                ],
+                [
+                    'id' => 22,
+                    'density_from' => 80,
+                    'density_to' => 100,
+                    'price' => 350, // Price per m³
+                ],
+                [
+                    'id' => 23,
+                    'density_from' => 50,
+                    'density_to' => 80,
+                    'price' => 330, // Price per m³
+                ],
+                [
+                    'id' => 24,
+                    'density_from' => 50,
+                    'density_to' => 1,
+                    'price' => 310, // Price per m³
+                ],
+            ],
+            9 => [  // fast car
+                [
+                    'id' => 1,
+                    'density_from' => 800,
+                    'density_to' => 800,
+                    'price' => 2.75,
+                ],
+                [
+                    'id' => 2,
+                    'density_from' => 600,
+                    'density_to' => 800,
+                    'price' => 2.80,
+                ],
+                [
+                    'id' => 3,
+                    'density_from' => 500,
+                    'density_to' => 600,
+                    'price' => 2.85,
+                ],
+                [
+                    'id' => 4,
+                    'density_from' => 400,
+                    'density_to' => 500,
+                    'price' => 2.90,
+                ],
+                [
+                    'id' => 5,
+                    'density_from' => 350,
+                    'density_to' => 400,
+                    'price' => 2.95,
+                ],
+                [
+                    'id' => 6,
+                    'density_from' => 300,
+                    'density_to' => 350,
+                    'price' => 3.00,
+                ],
+                [
+                    'id' => 7,
+                    'density_from' => 250,
+                    'density_to' => 300,
+                    'price' => 3.10,
+                ],
+                [
+                    'id' => 8,
+                    'density_from' => 200,
+                    'density_to' => 250,
+                    'price' => 3.20,
+                ],
+                [
+                    'id' => 9,
+                    'density_from' => 190,
+                    'density_to' => 200,
+                    'price' => 3.30,
+                ],
+                [
+                    'id' => 10,
+                    'density_from' => 180,
+                    'density_to' => 190,
+                    'price' => 3.40,
+                ],
+                [
+                    'id' => 11,
+                    'density_from' => 170,
+                    'density_to' => 180,
+                    'price' => 3.50,
+                ],
+                [
+                    'id' => 12,
+                    'density_from' => 160,
+                    'density_to' => 170,
+                    'price' => 3.60,
+                ],
+                [
+                    'id' => 13,
+                    'density_from' => 150,
+                    'density_to' => 160,
+                    'price' => 3.70,
+                ],
+                [
+                    'id' => 14,
+                    'density_from' => 140,
+                    'density_to' => 150,
+                    'price' => 3.80,
+                ],
+                [
+                    'id' => 15,
+                    'density_from' => 130,
+                    'density_to' => 140,
+                    'price' => 3.90,
+                ],
+                [
+                    'id' => 16,
+                    'density_from' => 120,
+                    'density_to' => 130,
+                    'price' => 4.00,
+                ],
+                [
+                    'id' => 17,
+                    'density_from' => 110,
+                    'density_to' => 120,
+                    'price' => 4.10,
+                ],
+                [
+                    'id' => 18,
+                    'density_from' => 100,
+                    'density_to' => 110,
+                    'price' => 4.20,
+                ],
+                [
+                    'id' => 19,
+                    'density_from' => 80,
+                    'density_to' => 100,
+                    'price' => 410, // Price per m³
+                ],
+                [
+                    'id' => 20,
+                    'density_from' => 50,
+                    'density_to' => 80,
+                    'price' => 390, // Price per m³
+                ],
+                [
+                    'id' => 21,
+                    'density_from' => 50,
+                    'density_to' => 1,
+                    'price' => 370, // Price per m³
+                ]
+            ]
+        ];
+    }
+
     public static function addPriceRangeToTypeDelivery(
         int $typeDeliveryId,
     ): ResultAnswer {
@@ -114,15 +402,16 @@ class OrderDeliveryPriceService extends PriceOutputService
         float $heightPerItem,
         float $depthPerItem,
         float $weightPerItem,
+        // int $categoryId,
         int $typeDeliveryId,
     ): float {
-        $density = self::calculateProductDensity(
-            $widthPerItem,
-            $heightPerItem,
-            $depthPerItem,
-            $weightPerItem,
-        );
 
+        // $density = self::calculateProductDensity(
+        //     $widthPerItem,
+        //     $heightPerItem,
+        //     $depthPerItem,
+        //     $weightPerItem,
+        // );
         // $densityPrice = self::getPriceByWeight($typeDeliveryId, $density);
 
         //new logic
@@ -134,7 +423,6 @@ class OrderDeliveryPriceService extends PriceOutputService
         $deliveryPrice = 0;
 
         if ($density > 100) {
-
             $totalWeight = ($itemsCount * $weightPerItemKg) + $packagingWeight;
             $densityPrice = self::getPriceByWeight($typeDeliveryId, $density);
             $deliveryPrice = $densityPrice * $totalWeight;
