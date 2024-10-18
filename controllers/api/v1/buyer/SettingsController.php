d86a5f507bce2e5a24294bd016ec138678f9a2be
Andrew <code70@inbox.ru>
2024-10-18 22:06:57 +0700

    some fixes/ remove set-categories block from buyer demo user

--- controllers/api/v1/buyer/SettingsController.php
+++ controllers/api/v1/buyer/SettingsController.php
@@ -26,7 +26,7 @@ class SettingsController extends BuyerController
         $behaviors['verbFilter']['actions']['set-delivery'] = ['put'];
         $behaviors['verbFilter']['actions']['set-categories'] = ['put'];
         array_unshift($behaviors['access']['rules'], [
-            'actions' => ['set-packaging'],
+            'actions' => [],
             'allow' => false,
             'matchCallback' => fn() => User::getIdentity()->role === User::ROLE_BUYER_DEMO,
         ]);
