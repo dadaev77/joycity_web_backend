aa5b00cb0dc1e351f77180a170b7efa40ee14a8a
Andrew <code70@inbox.ru>
2024-10-23 21:10:20 +0700

    fix Order Structure

--- models/structure/OrderStructure.php
+++ models/structure/OrderStructure.php
@@ -189,7 +189,7 @@ class OrderStructure extends Base
                 ['subcategory_id'],
                 'exist',
                 'skipOnError' => true,
-                'targetClass' => Category::class,
+                'targetClass' => \app\models\Category::class,
                 'targetAttribute' => ['subcategory_id' => 'id'],
             ],
             [
