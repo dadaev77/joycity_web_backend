2dbf61dd471ffa1b0b0745d60bcb3a61c38431fd
Andrew <code70@inbox.ru>
2024-10-23 17:56:26 +0700

    update catalog

---
+++ migrations/m241022_082325_update_type_delivery_price.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m241022_082325_update_type_delivery_price
+ */
+class m241022_082325_update_type_delivery_price extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m241022_082325_update_type_delivery_price cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m241022_082325_update_type_delivery_price cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
