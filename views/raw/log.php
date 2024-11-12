a125df755cf3a6493413057817d31b3d4399b060
Andrew <code70@inbox.ru>
2024-11-12 19:32:22 +0700

    remove backlog

--- views/raw/log.php
+++ views/raw/log.php
@@ -26,9 +26,11 @@
                 <div class="row">
                     <div class="col-12">
                         <ul class="nav nav-tabs" id="myTab" role="tablist">
+                            <!--
                             <li class="nav-item" role="presentation">
                                 <button class="nav-link active" id="backend-tab" data-bs-toggle="tab" data-bs-target="#backend" type="button" role="tab" aria-controls="backend" aria-selected="true">Backend Logs</button>
                             </li>
+                            -->
                             <li class="nav-item" role="presentation">
                                 <button class="nav-link" id="frontend-tab" data-bs-toggle="tab" data-bs-target="#frontend" type="button" role="tab" aria-controls="frontend" aria-selected="false">Frontend Logs</button>
                             </li>
@@ -47,11 +49,13 @@
                             </li>
                         </ul>
                         <div class="tab-content py-4" id="myTabContent">
+                            <!--
                             <div class="tab-pane fade show active" id="backend" role="tabpanel" aria-labelledby="backend-tab">
                                 <div style="white-space: wrap; word-break: break-all; font-size: 12px;">
-                                    <?= $logs ?>
+                                     < ? =  $logs   ? >
                                 </div>
                             </div>
+                            -->
                             <div class="tab-pane fade" id="frontend" role="tabpanel" aria-labelledby="frontend-tab">
                                 <div style="white-space: wrap; word-break: break-all; font-size: 12px;" id="frontend-logs">
                                     <div class="mb-4">
