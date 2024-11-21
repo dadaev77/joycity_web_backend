c850c5a9d13c95bb00117f94e1925e5f8caba495
Andrew <code70@inbox.ru>
2024-11-21 21:06:02 +0700

    upd buyerOffer validation

--- controllers/api/v1/buyer/order/BuyerOfferController.php
+++ controllers/api/v1/buyer/order/BuyerOfferController.php
@@ -77,32 +77,9 @@ class BuyerOfferController extends BuyerController
         $apiCodes = BuyerOffer::apiCodes();
 
         try {
+            $postData = Yii::$app->request->post();
             $user = User::getIdentity();
-            $params = POSTHelper::getPostWithKeys(
-                [
-                    'order_id',
-                    'price_product',
-                    // 'price_inspection',
-                    'total_quantity',
-                    'product_height',
-                    'product_width',
-                    'product_depth',
-                    'product_weight',
-                ],
-                true,
-            );
-            $notValidParams = POSTHelper::getEmptyParams($params, true);
-
-            if ($notValidParams) {
-                $errors = array_map(
-                    static fn($idx) => "Param `$notValidParams[$idx]` is empty",
-                    array_flip($notValidParams),
-                );
-
-                return ApiResponse::codeErrors($apiCodes->BAD_REQUEST, $errors);
-            }
-
-            $order = Order::findOne(['id' => $params['order_id']]);
+            $order = Order::findOne(['id' => $postData['order_id']]) ?? null;
 
             if (!$order) {
                 return ApiResponse::code($apiCodes->NOT_FOUND);
@@ -137,16 +114,16 @@ class BuyerOfferController extends BuyerController
                 'buyer_id' => $user->id,
                 'status' => BuyerOffer::STATUS_WAITING,
                 'price_product' => RateService::putInUserCurrency(
-                    $params['price_product'],
+                    $postData['price_product'],
                 ),
                 'price_inspection' => RateService::putInUserCurrency(
-                    $params['price_inspection'],
+                    $postData['price_inspection'],
                 ),
-                'total_quantity' => $params['total_quantity'],
-                'product_height' => $params['product_height'],
-                'product_width' => $params['product_width'],
-                'product_depth' => $params['product_depth'],
-                'product_weight' => $params['product_weight'],
+                'total_quantity' => $postData['total_quantity'],
+                'product_height' => $postData['product_height'],
+                'product_width' => $postData['product_width'],
+                'product_depth' => $postData['product_depth'],
+                'product_weight' => $postData['product_weight'],
                 'currency' => $user->settings->currency,
             ]);
 
@@ -180,7 +157,7 @@ class BuyerOfferController extends BuyerController
         } catch (Throwable $e) {
             isset($transaction) && $transaction->rollBack();
 
-            return ApiResponse::internalError($e);
+            return ApiResponse::internalError($e->getMessage());
         }
     }
 
