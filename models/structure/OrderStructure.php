40b61fb4e47b3bbb292e5eb7d9b1370987ad4cb3
Andrew <code70@inbox.ru>
2024-11-21 18:17:14 +0700

    upd order validation

--- models/structure/OrderStructure.php
+++ models/structure/OrderStructure.php
@@ -149,8 +149,9 @@ class OrderStructure extends Base
                 ],
                 'required',
             ],
-            ['amount_of_space', 'integer', 'min' => 1, 'max' => 6, 'skipOnEmpty' => true, 'allowEmptyString' => true],
             ['amount_of_space', 'default', 'value' => null],
+            ['amount_of_space', 'integer', 'min' => 1, 'max' => 6, 'skipOnEmpty' => true],
+            ['amount_of_space', 'safe'],
             ['cargo_number', 'string', 'max' => 255],
             [['created_at'], 'safe'],
             [
