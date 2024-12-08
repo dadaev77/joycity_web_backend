06ff49513c950e2f9978ff63042ae69721746e4b
Andrew <code70@inbox.ru>
2024-12-08 18:35:42 +0700

    upd register telegram condition

--- services/output/OrderDistributionOutputService.php
+++ services/output/OrderDistributionOutputService.php
@@ -4,6 +4,7 @@ namespace app\services\output;
 
 use app\helpers\ModelTypeHelper;
 use app\models\OrderDistribution;
+use app\services\RateService;
 use app\services\UserActionLogService as Log;
 use Yii;
 
@@ -24,19 +25,18 @@ class OrderDistributionOutputService extends OutputService
 
         return array_map(static function ($model) {
             $info = ModelTypeHelper::toArray($model);
-
             $subcategory = $model->order->subcategory;
             $category = $subcategory->category;
             $appLang = Yii::$app->user->getIdentity()->settings->application_language;
-
+            $userCurrency = Yii::$app->user->getIdentity()->settings->currency;
             $lang = match (strtolower($appLang)) {
                 'ru' => 'ru_name',
                 'en' => 'en_name',
                 'zh' => 'zh_name',
+                default => 'ru_name',
             };
-
             $info['order']['product_name'] = $category ? $category->{$lang} . ' / ' . $subcategory->{$lang} : $subcategory->{$lang};
-
+            $info['order']['expected_price_per_item'] = RateService::convertValue($info['order']['expected_price_per_item'], $info['order']['currency'], $userCurrency);
             unset(
                 $info['order_id'],
                 $info['buyer_ids_list'],
