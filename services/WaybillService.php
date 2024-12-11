407102859927f8c2de2a5ae70560f8c2d0a71f40
Andrew <code70@inbox.ru>
2024-12-11 19:50:07 +0700

    upd

--- services/WaybillService.php
+++ services/WaybillService.php
@@ -181,7 +181,7 @@ class WaybillService
         }
 
         $waybillAttachment = base64_encode(file_get_contents(Yii::getAlias('@webroot') . $waybillAttachment));
-
+        Log::info('Прикрепленный файл: ' . $waybillAttachment);
         // Расчет объема
         $volume = isset($bdo->product_height, $bdo->product_width, $bdo->product_depth, $bdo->amount_of_space)
             ? ($bdo->product_height / 100) * ($bdo->product_width / 100) * ($bdo->product_depth / 100) * $bdo->amount_of_space
