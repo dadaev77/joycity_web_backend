5ce79e9cd399ff74f5d1c97aebc9b7d39d1ee730
Andrew <code70@inbox.ru>
2025-01-21 19:14:42 +0700

    test price overall

--- services/output/OrderOutputService.php
+++ services/output/OrderOutputService.php
@@ -160,7 +160,7 @@ class OrderOutputService extends OutputService
             $info['price'] = OrderPrice::calculateOrderPrices($info['id'], $userCurrency);
 
             if ($info['buyerOffer']) {
-                $info['price']['product']['overall'] = $info['buyerOffer']['price_product'] * $info['buyerOffer']['total_quantity'];
+                $info['price']['product_overall'] = $info['buyerOffer']['price_product'] * $info['buyerOffer']['total_quantity'];
             }
             unset(
                 // $info['created_at'],
