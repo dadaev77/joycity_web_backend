c022ddb58c6ba7a937136bbc746e5a2838cd9423
Andrew <code70@inbox.ru>
2025-02-02 19:47:45 +0700

    add authorized user id in answer

--- controllers/api/v1/ChatController.php
+++ controllers/api/v1/ChatController.php
@@ -72,7 +72,7 @@ class ChatController extends V1Controller
                     'telegram' => $user->telegram,
                 ];
             }
-            $chat->auth_user_id = $userId;
+            $chat['auth_user_id'] = $userId;
             $chat->metadata = $metadata;
         }
 
