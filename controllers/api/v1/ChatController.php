f43ab223bbd086d287d4653189c4cbe86784a239
Andrew <code70@inbox.ru>
2025-02-03 22:36:40 +0700

    upd log

--- controllers/api/v1/ChatController.php
+++ controllers/api/v1/ChatController.php
@@ -227,7 +227,7 @@ class ChatController extends V1Controller
     public function actionSendMessage()
     {
         Yii::$app->telegramLog->send('success', json_encode(Yii::$app->request->post()), 'dev');
-        Yii::log(json_encode(Yii::$app->request->post()));
+        Yii::info(json_encode(Yii::$app->request->post()));
         return;
         $chatId = Yii::$app->request->post('chat_id');
         $content = Yii::$app->request->post('content');
