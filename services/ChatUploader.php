d81f8e34417c3fdc2612970dc53d0999b04480ec
Andrew <code70@inbox.ru>
2025-02-03 23:09:00 +0700

    upd log

--- services/ChatUploader.php
+++ services/ChatUploader.php
@@ -8,26 +8,18 @@ class ChatUploader
 
     public static function uploadImages(array $images)
     {
-        return [
-            'images' => $images,
-        ];
+        return $images;
     }
     public static function uploadVideos(array $videos)
     {
-        return [
-            'videos' => $videos,
-        ];
+        return $videos;
     }
     public static function uploadFiles(array $files)
     {
-        return [
-            'files' => $files,
-        ];
+        return $files;
     }
     public static function uploadAudios(array $audio)
     {
-        return [
-            'audio' => $audio,
-        ];
+        return $audio;
     }
 }
