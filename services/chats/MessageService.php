d6a2a6d0b9aa6a005492ef7fa6e274a8a2f21c87
Andrew Master <andrewmaster@Andrews-MacBook-Pro.local>
2025-02-06 23:01:37 +0700

    temp

--- services/chats/MessageService.php
+++ services/chats/MessageService.php
@@ -127,20 +127,14 @@ class MessageService
      */
     private static function translateMessage($text)
     {
-        // $translator = new \app\services\TranslationService();
-        // $result = $translator->translate($text);
-        // $translateResult = $result->result;
+        $translator = new \app\services\TranslationService();
+        $result = $translator->translate($text);
+        $translateResult = $result->result;
         
-        // return [
-        //     'en' => $translateResult['en'],
-        //     'ru' => $translateResult['ru'],
-        //     'zh' => $translateResult['zh'],
-        // ];
-
         return [
-            'en' => $text,
-            'ru' => $text,
-            'zh' => $text,
+            'en' => $translateResult['en'],
+            'ru' => $translateResult['ru'],
+            'zh' => $translateResult['zh'],
         ];
     }
 
