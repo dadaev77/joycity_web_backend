9afbadbd0760b0ee8bc3ad07ec6fceb90ebbaa79
Andrew Master <andrewmaster@Andrews-MacBook-Pro.local>
2025-02-08 19:42:51 +0700

    upd search chats

--- controllers/api/v1/ChatController.php
+++ controllers/api/v1/ChatController.php
@@ -173,8 +173,6 @@ class ChatController extends V1Controller
             ];
         }
 
-        Yii::$app->telegramLog->send('info', json_encode($data), 'dev');
-
         return [
             'auth_user_id' => User::getIdentity()->id,
             'chats' => $data
