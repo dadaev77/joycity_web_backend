16a6fa8d76faa718397cd1ae71a7e218c2046e04
Andrew Master <andrewmaster@Andrews-MacBook-Pro.local>
2025-02-10 20:55:31 +0700

    update websocket service

--- services/WebsocketService.php
+++ services/WebsocketService.php
@@ -9,13 +9,12 @@ class WebsocketService
 {
     public static function sendNotification(array $notification)
     {
-        // todo Notification model
         $client = new \GuzzleHttp\Client();
         $response = $client->post(self::getWebsocketUrl() . '/notification/send', [
             'json' => ['notification' => $notification],
             'headers' => ['Content-Type' => 'application/json']
         ]);
-
+        Yii::$app->telegramLog->send('success', 'Send notification');
         if ($response->getBody()->getContents() !== 'ok') {
             return Result::error();
         }
