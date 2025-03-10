79bdebf371c2284ff082a1e83f3833b0ca806d6d
Andrew Master <code70@inbox.ru>
2025-03-10 21:25:16 +0700

    upd

--- controllers/RawController.php
+++ controllers/RawController.php
@@ -190,6 +190,12 @@ class RawController extends Controller
         $user_id = Yii::$app->request->post('user_id');
         $message = Yii::$app->request->post('message');
 
-        return \app\services\push\PushService::sendPushNotification($user_id, $message);
+        \app\services\push\PushService::sendPushNotification($user_id, $message);
+
+        \app\services\push\PushService::sendPushNotification($user_id, $message, $token->push_token);
+
+        $tokens = \app\models\PushNotification::find()->where(['client_id' => $user_id])->select('push_token')->all();
+
+        return $tokens;
     }
 }
