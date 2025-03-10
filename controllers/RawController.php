c305edf9adabb9b12e72b79dc6c8fa1483421902
Andrew Master <code70@inbox.ru>
2025-03-10 16:42:10 +0700

    update cron task creating

--- controllers/RawController.php
+++ controllers/RawController.php
@@ -226,15 +226,19 @@ class RawController extends Controller
     public function actionTest2()
     {
         $order_distribution = new \app\models\OrderDistribution();
+
         $order_distribution->loadDefaultValues();
+
         $order_distribution->setAttributes([
-            'order_id' => 115,
+            'order_id' => 222,
             'current_buyer_id' => 1,
             'requested_at' => date('Y-m-d H:i:s'),
             'status' => \app\models\OrderDistribution::STATUS_IN_WORK,
             'buyer_ids_list' => '1,2,3',
         ]);
+
         $order_distribution->save();
+
         return $order_distribution;
     }
 }
