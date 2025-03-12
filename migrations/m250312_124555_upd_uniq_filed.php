c20df35b9e63b95dad96c9c6c40f72790ff86c34
Andrew Master <code70@inbox.ru>
2025-03-12 19:55:54 +0700

    upd

--- migrations/m250312_124555_upd_uniq_filed.php
+++ migrations/m250312_124555_upd_uniq_filed.php
@@ -14,7 +14,6 @@ class m250312_124555_upd_uniq_filed extends Migration
     {
         // Удаляем внешнее ограничение, использующее индекс
         $this->dropForeignKey('fk_order_distribution_order_id', 'order_distribution');
-        $this->dropIndex('order_id', 'order_distribution');
     }
 
     /**
@@ -22,10 +21,6 @@ class m250312_124555_upd_uniq_filed extends Migration
      */
     public function safeDown()
     {
-        // Восстанавливаем уникальный индекс с поля order_id
-        $this->createIndex('order_id', 'order_distribution', 'order_id', true);
-
-        // Восстанавливаем внешнее ограничение
         $this->addForeignKey('fk_order_distribution_order_id', 'order_distribution', 'order_id', 'order', 'id', 'CASCADE', 'CASCADE');
     }
 
