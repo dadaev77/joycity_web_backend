4ee46a7d3dd61564bc224f541a4954bf41970740
Аслан Дадаев <a.dadev@friflex.com>
2025-03-24 13:00:43 +0300

    JOYCITY-567: added OrderDeliveryTimeService

--- migrations/m240000_000001_update_delivery_time_days.php
+++ migrations/m240000_000001_update_delivery_time_days.php
@@ -9,13 +9,15 @@ class m240000_000001_update_delivery_time_days extends Migration
 {
     public function up()
     {
-        // Обновляем время доставки для медленного авто (25 дней)
+        // Сначала добавляем колонку
+        $this->addColumn('type_delivery', 'delivery_time_days', $this->integer()->null());
+
+        // Затем обновляем значения
         $this->update('type_delivery', 
             ['delivery_time_days' => 25],
             ['id' => 8] // Медленное авто
         );
 
-        // Обновляем время доставки для быстрого авто (16 дней)
         $this->update('type_delivery', 
             ['delivery_time_days' => 16],
             ['id' => 9] // Быстрое авто
@@ -24,10 +26,7 @@ class m240000_000001_update_delivery_time_days extends Migration
 
     public function down()
     {
-        // Возвращаем значения в NULL
-        $this->update('type_delivery', 
-            ['delivery_time_days' => null],
-            ['id' => [8, 9]]
-        );
+        // Удаляем колонку
+        $this->dropColumn('type_delivery', 'delivery_time_days');
     }
 } 
