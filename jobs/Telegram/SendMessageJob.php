2726a28ad596beec1f39179228b75d91f93790b5
Andrew Master <code70@inbox.ru>
2025-03-27 13:48:23 +0700

    exclude metadata from afterSave

--- jobs/Telegram/SendMessageJob.php
+++ jobs/Telegram/SendMessageJob.php
@@ -23,8 +23,13 @@ class SendMessageJob extends BaseObject implements JobInterface
     {
         try {
             Yii::$app->telegramLog->send($this->type, $this->message, $this->env, $this->async);
-            echo "\n" . "\033[32m" . '[Telegram] Отправлено в телеграм' . "\033[0m";
-            var_dump($this->message);
+            echo "\n" . "\033[32m" . '[Telegram] Отправлено в телеграм' . "\033[0m \n";
+            var_dump([
+                'type' => $this->type,
+                'message' => $this->message,
+                'env' => $this->env,
+                'async' => 'queue',
+            ]);
             echo "\n" . "\033[32m" . '[Telegram] Конец сообщения' . "\033[0m";
         } catch (Exception $e) {
             echo "\n" . "\033[31m" . '[Telegram] Ошибка отправки в телеграм: ' . $e->getMessage() . "\033[0m";
