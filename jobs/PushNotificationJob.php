b239b7b586b7f075b4d3a48afff2c94261464a90
Andrew Master <code70@inbox.ru>
2025-03-28 21:29:04 +0700

    upd

--- jobs/PushNotificationJob.php
+++ jobs/PushNotificationJob.php
@@ -16,13 +16,28 @@ class PushNotificationJob extends BaseObject implements JobInterface
     public function execute($queue)
     {
         try {
-            $send = PushService::sendPushNotification($this->user_id, $this->message, false);
-            if (!$send) {
-                Yii::error('Ошибка при отправке push уведомления: ' . $send);
+            $user = \app\models\User::findOne($this->user_id);
+            echo "\n" . "\033[38;5;214m" . "[PT Count]  {$this->user_id}: " . count($user->pushTokens) . "\n" . "\033[0m";
+            foreach ($user->pushTokens as $pushToken) {
+                echo "\n" . "\033[31m" . "--[PT:{$this->user_id}] " . $pushToken->push_token . "\033[0m";
+                echo "\n" . "\033[38;5;214m" . "   [PT:OS] " . $pushToken->operating_system . "\033[0m";
+                echo "\n" . "\033[38;5;214m" . "   [PT:USER] " . $user->id . "\033[0m";
+                echo "\n" . "\033[38;5;214m" . "   [PT:MESSAGE] " . $this->message . "\033[0m";
+
+                if ($pushToken->operating_system === 'ios') {
+                    $pushToken->badge_count++;
+                    $pushToken->save();
+                }
+                \app\services\push\FirebaseService::sendPushNotification(
+                    $this->user_id,
+                    $this->message,
+                    $pushToken->push_token,
+                    $pushToken->operating_system
+                );
             }
-        } catch (Exception $e) {
-            Yii::error('Ошибка при отправке push уведомления: ' . $e->getMessage());
-            echo "\n" . "\033[31m" . 'Ошибка при отправке push уведомления: ' . $e->getMessage() . "\033[0m";
+        } catch (\Exception $e) {
+            Yii::error("Push notification error: " . $e->getMessage(), 'push');
+            return $e->getMessage();
         }
     }
 }
