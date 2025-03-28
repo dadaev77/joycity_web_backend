0b616c110fed0a4f55954bd018dfd2f1db7d800a
Andrew Master <code70@inbox.ru>
2025-03-28 19:44:45 +0700

    upd

--- services/push/PushService.php
+++ services/push/PushService.php
@@ -129,8 +129,9 @@ class PushService
 
         try {
             $user = User::findOne($user_id);
-            echo "\n" . "\033[38;5;214m" . "Количество токенов для пользователя {$user_id}: " . count($user->pushTokens) . "\n" . "\033[0m";
+            echo "\n" . "\033[38;5;214m" . "[PT] Количество токенов для пользователя {$user_id}: " . count($user->pushTokens) . "\n" . "\033[0m";
             foreach ($user->pushTokens as $pushToken) {
+                echo "\n" . "\033[31m" . "[PT] " . $pushToken->push_token . "\033[0m";
                 if ($pushToken->operating_system === 'ios') {
                     $pushToken->badge_count++;
                     $pushToken->save();
