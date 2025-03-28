0f64c26d02622fec6080c6ec5181dd5203b964fb
Andrew Master <code70@inbox.ru>
2025-03-28 22:13:37 +0700

    upd

--- jobs/FirebaseJob.php
+++ jobs/FirebaseJob.php
@@ -17,7 +17,6 @@ class FirebaseJob extends BaseObject implements JobInterface
     public function execute($queue)
     {
         echo "\n" . "\033[38;5;214m" . "************************************************" . "\033[0m";
-        echo "\n" . "\033[38;5;214m" . "   [FJ:MESSAGE] " . $this->message . "\033[0m";
         echo "\n" . "\033[38;5;214m" . "   [FJ:PUSH_TOKEN] " . $this->pushToken . "\033[0m";
         echo "\n" . "\033[38;5;214m" . "   [FJ:OS] " . $this->os . "\033[0m";
         echo "\n" . "\033[38;5;214m" . "************************************************" . "\033[0m";
