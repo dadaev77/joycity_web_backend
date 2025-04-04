0790581e08c1d84c9bc5f7bc54c8390a995484e1
Andrew Master <code70@inbox.ru>
2025-04-04 17:41:27 +0700

    upd

--- services/RoleService.php
+++ services/RoleService.php
@@ -66,8 +66,8 @@ class RoleService
         try {
             $added = 0;
             foreach ($users as $user) {
-                $user->role_id = $roles[$user['role']];
-                $user->save();
+                $user['role_id'] = $roles[$user['role']];
+                Yii::$app->db->createCommand('UPDATE user SET role_id = :role_id WHERE id = :id')->bindValue(':role_id', $user['role_id'])->bindValue(':id', $user['id'])->execute();
                 $added++;
             }
             if ($added > 0) {
