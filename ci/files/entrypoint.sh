02c15e455f45ee6be6ab59171c4fdb08ea857cd1
Никита Видяев <n.vidyaev@friflex.com>
2025-04-08 09:51:36 +0000

    Update 4 files
    - /ci/files/apache2/default.conf
    - /ci/files/apache2/mpm_prefork.conf
    - /ci/files/entrypoint.sh
    - /ci/Dockerfile

---
+++ ci/files/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+chown server:server /app/entrypoint/api/attachments
+echo "chown attachments done"
+
+sleep 5
+cron
+su -c 'crontab /app/components/docker/cron_tasks.txt' -s /bin/bash server
+echo "Cron tasks started"
+
+sleep 5
+su -c 'php /app/yii migrate --interactive=0' -s /bin/bash server
+echo "Migration done"
+
+su -c 'mkdir /app/entrypoint/api/uploads/chats' -s /bin/bash root
+su -c 'chown server:server /app/entrypoint/api/uploads/chats' -s /bin/bash root
+echo "Chats directory created"
+su -c 'php yii deploy-prod/all' -s /bin/bash server
+
+su -c 'php yii push-queue/listen &' -s /bin/bash server
+echo "Start push-queue"
+su -c 'php yii queue/listen &' -s /bin/bash server
+echo "Start queue"
+#su -c 'php yii deploy-prod/all' -s /bin/bash server
